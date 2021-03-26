74072c4aa650497ca6cd55b31f42f4f95ff16e9e
Christoph <christoph.bodner@gmail.com>
2021-03-26 15:13:21 +0100

    implement rule of 5 chatbot

--- src/chatbot.h
+++ src/chatbot.h
@@ -3,6 +3,7 @@
 
 #include <wx/bitmap.h>
 #include <string>
+#include <iostream>
 
 class GraphNode; // forward declaration
 class ChatLogic; // forward declaration
@@ -29,7 +30,81 @@ public:
 
     //// STUDENT CODE
     ////
+    // copy constructor
+    ChatBot(const ChatBot &source) {
 
+        //deep copy (owned)
+        // TODO: If we want to make a deep copy: Does it work like this?
+        //_image = new wxBitmap(*source._image);
+        *_image = *source._image;
+        //shallow copys (not owned)
+        _currentNode = source._currentNode;
+        _rootNode = source._rootNode;
+        _chatLogic = source._chatLogic;
+        std::cout << "ChatBot Copy Constructor" << std::endl;
+    }
+
+    //copy assignment operator
+    ChatBot &operator=(const ChatBot &source) {
+        if (this == &source) {
+            return *this;
+        }
+        //owned
+        delete _image;
+        // _image = new wxBitmap(*source._image);
+        //assign data from source
+        *_image = *source._image;
+
+        //not owned
+        _currentNode = source._currentNode;
+        _rootNode = source._rootNode;
+        _chatLogic = source._chatLogic;
+
+        std::cout << "ChatBot Copy Assignment Operator" << std::endl;
+
+        return *this;
+
+    }
+
+    //move constructor
+    ChatBot(ChatBot &&source) {
+        std::cout << "MOVING instance " << &source << " to instance " << this << std::endl;
+
+        *_image = *source._image;
+        _currentNode = source._currentNode;
+        _rootNode = source._rootNode;
+        _chatLogic = source._chatLogic;
+        
+        source._image = nullptr;
+        source._currentNode = nullptr;
+        source._rootNode = nullptr;
+        source._chatLogic = nullptr;
+
+    }
+
+    //move assignment operator
+    ChatBot &operator=(ChatBot &&source) {
+        std::cout << "MOVING (assign) instance " << &source << " to instance " << this << std::endl;
+        if (this == &source)
+            return *this;
+
+        //owned
+        delete _image;
+        //_image = new wxBitmap(*source._image);
+        *_image = *source._image;
+
+        //not owned
+        _currentNode = source._currentNode;
+        _rootNode = source._rootNode;
+        _chatLogic = source._chatLogic;
+        
+        source._image = nullptr;
+        source._currentNode = nullptr;
+        source._rootNode = nullptr;
+        source._chatLogic = nullptr;        
+
+        return *this;
+    }
     ////
     //// EOF STUDENT CODE
 
